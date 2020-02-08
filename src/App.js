4dddf56b10ad8cc542f15b99c11b148cd93f7b96
LESANF <nagong1000@naver.com>
2020-02-09 02:03:00 +0900

    docs: Styled-components ING

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BigFrame>
+        <Apple>CLEAN</Apple>
+        <Apple danger>DANGER</Apple>
+      </BigFrame>
+    );
+  }
 }
+
+const BigFrame = styled.div`
+  background-color: pink;
+  height: 100vh;
+`;
+
+const Apple = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    background-color: peru;
+  }
+  background-color: ${props => (props.danger ? "red" : "green")};
+`;
 
 export default App;
